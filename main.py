9b0b106f1334aa890f68f47e27594976af4a22a9
Denys Tantsiura <ghost0002501@gmail.com>
2023-02-01 00:12:02 +0200

    HomeWork done.

--- main.py
+++ main.py
@@ -1,99 +1,89 @@
 from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
-import mimetypes  # Для визначення MIME types файлів у Python існує окремий модуль
+import mimetypes  # окремий модуль для визначення MIME types файлів
 import pathlib
 import urllib.parse
-# import urllib.request  # багато недоліків на відміну від 3party: request
 import socket
 from threading import Thread
 
+
+MESSAGE_LENGTH = 1024
 UDP_IP = '127.0.0.1'
-UDP_PORT = 5000  # http://127.0.0.1:5000/
+UDP_PORT = 5000  # http://localhost:5000/
+HTTP_IP = ''  # Якщо аргумент для адреси порожнй рядок - то доступний для всіх інтерфейсів(ip)
+HTTP_PORT = 3000
 file_data = 'data.json'
 path_data = pathlib.Path('storage')
-"""
-Наприклад, при зверненні до маршруту /contact у змінній self.path 
-знаходитиметься значення /contact. І в принципі для нашого простого 
-прикладу цього достатньо. Але правильнішим буде підхід використання 
-спеціальної функції urllib.parse.urlparse, яка повертає 
-об'єкт 
-ParseResult(scheme='', netloc='', path='/contact', params='', query='', 
-fragment=''). 
-Вона може отримувати з маршруту, також наприклад 'query' запити. 
-Якщо ми звернемося за адресою 
-/contact?name=Test 
-у змінній self.path буде знаходитись 
-/contact?name=Test, 
-а ось urllib.parse.urlparse поверне нам те, що потрібно 
-ParseResult(scheme='', netloc='', path='/contact', params='', 
-query='name=Test', fragment='') 
-та в атрибуті path буде маршрут '/contact', 
-а 'query' містить значення 'name=Test' 
-куди воно і мало потрапити.
-"""
+file_path = pathlib.Path(path_data, file_data)  # path_data / file_data
+
+
 class HttpHandler(BaseHTTPRequestHandler):  # оброблювач запитів
     """простий HTTP сервер, який приймає запити за адресами:
      / та /message, і на GET-запит відповідає відповідними сторінками,
      а на POST-запит - див.комент нижче."""
-    def do_GET(self):
-        pr_url = urllib.parse.urlparse(self.path)  # повертає об'єкт ParseResult(scheme='', netloc='', path='/contact',
-        # params='', query=..., fragment='').
-        # Сама маршрутизація виконана за допомогою вкладених інструкцій if:
-        if pr_url.path == '/':
-            self.send_html_file('index.html')
-        elif pr_url.path == '/message':
-            self.send_html_file('message.html')
-        else:  # додамо до коду маршрутизації обробку статичних ресурсів:
-            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
-                self.send_static()  # Надсилаємо статичні ресурси (картинки, style.css, others...)
-            else:  # Якщо жоден маршрут не визначено, ми повертаємо спеціальну сторінку для помилки 404 Not Found.
-                self.send_html_file('error.html', 404)
+    def do_GET(self) -> None:
+        # повертає об'єкт ParseResult(scheme='', netloc='', path='/message', params='', query=..., fragment=''):
+        pr_url = urllib.parse.urlparse(self.path)  
 
-    # Для відповіді браузеру ми використовуємо метод send_html_file:
-    def send_html_file(self, filename, status=200):  # default status = 200
+        match pr_url.path:
+            case '/': 
+                self.send_html_file('index.html')
+            case '/message':
+                self.send_html_file('message.html')
+            case _:
+                # додамо до коду маршрутизації обробку статичних ресурсів:
+                if pathlib.Path().joinpath(pr_url.path[1:]).exists():
+                    self.send_static()  # Надсилаємо статичні ресурси (картинки, style.css, others...)
+                else:
+                    self.send_html_file('error.html', 404)
+
+    def send_html_file(self, filename: str, status: int = 200) -> None:  # default status = 200
+        """Для відповіді браузеру ми використовуємо метод send_html_file."""
         self.send_response(status)
         # При надсиланні html файлу ми повідомили браузеру контент надсиланням наступного заголовка:
-        self.send_header('Content-type', 'text/html')  #  MIME types файлу. Так ми повідомляємо браузеру тип даних, які можуть бути передані за допомогою HTTP протоколу.
-        self.end_headers()
+        self.send_header('Content-type', 'text/html')  # MIME types файлу - повідомляємо браузеру тип даних,
+        # які можуть бути передані за допомогою HTTP протоколу.
+        self.end_headers()  # send end-headers to show ending request
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
 
-    # Для визначення MIME types файлів у Python існує окремий модуль mimetypes:
-    # Картинки та стилі з файлу style.css, які повинні повертатися з сервера,
-    # але не є файлами HTML, називають загальним словом статичні ресурси.
-    def send_static(self):
+    def send_static(self) -> None:
+        """
+        Для визначення MIME types файлів у Python існує окремий модуль mimetypes:
+        Картинки та стилі з файлу style.css, які повинні повертатися з сервера,
+        але не є файлами HTML, називають загальним словом статичні ресурси.
+        """
         self.send_response(200)
-        mt = mimetypes.guess_type(self.path)
-        if mt:  # Якщо модуль визначив тип файлу, то надсилаємо його:
-            self.send_header("Content-type", mt[0])
+        mime_type = mimetypes.guess_type(self.path)
+        if mime_type:  # Якщо модуль визначив тип файлу, то надсилаємо його:
+            self.send_header('Content-type', mime_type[0])
+
         else:  # У разі невдачі ми вважаємо тип файлу простим текстом:
-            self.send_header("Content-type", 'text/plain')
-        self.end_headers()
+            self.send_header('Content-type', 'text/plain')
+        self.end_headers()  # send end-headers to show ending request
         with open(f'.{self.path}', 'rb') as file:
             self.wfile.write(file.read())
 
     """
     Обробка форми виконується функцією do_POST. 
     Оскільки в нашому додатку лише одна форма, 
-    то ми не писатимемо всередині цієї функції обробку маршрутів. 
-    У self.path завжди буде /contact, який прописаний у 
-    атрибуті action нашої форми на сторінці contact.html.
+    то не писатимемо всередині цієї функції обробку маршрутів. 
+    У self.path завжди буде /message, який прописаний у 
+    атрибуті action цієї форми на сторінці message.html.
     """
-    def do_POST(self):  # при натисканні на веб-сторінці Send
+    def do_POST(self) -> None:  # при натисканні на веб-сторінці Send
         """
+        Обробка форми.
         Для отримання даних у додатку з форми ми використовуємо функцію
         self.rfile.read, яка читає байт-рядок певного розміру. Розмір
         даних у байтах браузер (клієнт) передає через заголовок Content-Length.
         Тому наступним рядком коду ми отримуємо дані від браузера:
         """
         data = self.rfile.read(int(self.headers['Content-Length']))
-        print(data)  # Це байт-рядок виду: b'username=krabaton&email=krabat%40test.com&message=Hello+my+friend' :
-        """"Для форми з enctype="application/x-www-form-urlencoded" пробіли повинні бути замінені на "+", 
-        а також браузер застосовує до рядка метод encodeURIComponent ."""
-        # Щоб повернути дані до початкового вигляду, нам треба застосувати метод urllib.parse.unquote_plus:
-        # data_parse = urllib.parse.unquote_plus(data.decode())
-        # пересилає його(байт-рядок) далі на обробку за допомогою socket (протокол UDP), Socket серверу.
+        # data- Це байт-рядок виду: b'username=den&email=den%40test.com&message=Hello+my+friend.'
+
+        # пересилає його(байт-рядок) далі на обробку за допомогою socket (протокол UDP), Socket серверу:
         self._send_to_form_handler(UDP_IP, UDP_PORT, data)
    
         """
@@ -105,30 +95,36 @@ class HttpHandler(BaseHTTPRequestHandler):  # оброблювач запиті�
         self.send_header('Location', '/')
         self.end_headers()  # send end-headers to show ending request
 
-    def _send_to_form_handler(self, ip, port, data):
+    @staticmethod
+    def _send_to_form_handler(ip: str, port: int, data: bytes) -> None:
+        # Пересилаємо Socket серверу data
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         server = ip, port
         sock.sendto(data, server)
-        # print(f'Send data: {data.decode()} to server: {server}')
-        # response, address = sock.recvfrom(1024)
-        # print(f'Response data: {response.decode()} from address: {address}')
         sock.close()
 
 
-def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
-    # на якому порті та адресі прийматимемо/очікуємо з'єднання:
-    server_address = ('', 3000)  # порт 3000, Якщо аргумент для адреси порожнй рядок - то доступний для всіх інтерфейсів
+def run_http_server(server_class=HTTPServer, handler_class=HttpHandler) -> None:
+    """Запускаємо сервер, перед цим вказуємо на якому порті та адресі прийматимемо/очікуємо з'єднання:"""
+    server_address = (HTTP_IP, HTTP_PORT)
     http = server_class(server_address, handler_class)  # сам запит
     try:
-        http.serve_forever()  # буде працювати доки не викличемо ШатДаун
+        http.serve_forever()  # сервер буде працювати доки не викличемо 'примусово' (shutdown)
+
     except KeyboardInterrupt:  # зупиняємо сервер за виключенням (прериванням клави ctrl+c напирклад)
         http.server_close()
 
 
-def run_form_handler_server(ip, port):
-    # використовується модуль socket:
-    # Повертає об'єкт-інтерфейс для комунікації через який. 
-    # І клієнт і сервер мають мати інстанси сокета.
+def run_form_handler_server(ip: str, port: int) -> None:
+    """
+    Socket сервер переводить отриманий байт-рядок у словник
+    і зберігає його в json файл data.json в папку storage.
+    """
+    
+    """
+    Використовується модуль socket:
+    Повертає об'єкт-інтерфейс для комунікації через який. 
+    І клієнт і сервер мають мати інстанси сокета."""
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     """
@@ -145,59 +141,55 @@ def run_form_handler_server(ip, port):
 
     try:
         while True:
-            ## ///Socket сервер переводить отриманий байт-рядок у словник 
-            ## ///і зберігає його в json файл data.json в папку storage.
-            data, address = sock.recvfrom(1024)  # отримуємо 1024??? байта
-            print(f'Received data: {data.decode()} from: {address}')
-            print(data)  # Це байт-рядок виду: b'username=krabaton&email=krabat%40test.com&message=Hello+my+friend' :
-            """"Для форми з enctype="application/x-www-form-urlencoded" пробіли повинні бути замінені на "+", 
-            а також браузер застосовує до рядка метод encodeURIComponent ."""
-            # Щоб повернути дані до початкового вигляду, нам треба застосувати метод urllib.parse.unquote_plus:
-            data_parse = urllib.parse.unquote_plus(data.decode())
+            data, address = sock.recvfrom(MESSAGE_LENGTH)  # отримуємо MESSAGE_LENGTH байта (1024)
+            # data - Це байт-рядок виду: b'username=den&email=den%40test.com&message=Hello+my+friend.'
 
-            # print(data_parse)  # username=krabaton&email=krabat@test.com&message=Hello my friend
-            # Після цього рядок можна перетворити на словник таким виразом:
+            """"Для форми з enctype="application/x-www-form-urlencoded" пробіли повинні бути замінені на "+", 
+            а також браузер застосовує до рядка метод encodeURIComponent."""
+            # Щоб повернути дані до початкового вигляду, треба застосувати метод urllib.parse.unquote_plus:
+            data_parse = urllib.parse.unquote_plus(data.decode())
+            # data_parse=> username=den&email=den@test.com&message=Hello my friend.
+            
+            # Після цього рядок можна перетворити на словник:
             data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
-            print(data_dict)  # {'username': 'krabaton', 'email': 'krabat@test.com', 'message': 'Hello my friend'}
-            data_dict = prepare_data(data_dict)
-            save_data(data_dict)
+            # data_dict=> {'username': 'den', 'email': 'den@test.com', 'message': 'Hello my friend.'}
+
+            data_dict = prepare_data(data_dict, file_path)
+            save_data(data_dict, file_path)
 
     except KeyboardInterrupt:
-        print(f'Destroy server')
+        print(f'Destroy the server.')
 
     finally:
         sock.close()
 
 
-def prepare_data(data_dict):
-    # convert to json...
-    if (path_data / file_data).exists():
-        with open(path_data / file_data, "r") as fh:
-            data_json = json.load(fh)
+def prepare_data(data_dict: dict, path_of_file: pathlib.Path) -> dict:
+    """
+    Read the existing data and convert the new data to json, 
+    then add the new json data to the existing data and return it.
+    """
+    data_json = {}
+    if path_of_file.exists():
+        with open(path_of_file, 'r') as fh:
+            data_json = json.load(fh)  # try-except forbidden file ...
 
-    else:
-        data_json = {}
-
-    new_data_json = {str(datetime.now()):data_dict}
+    new_data_json = {str(datetime.now()): data_dict}
     data_json.update(new_data_json)
 
     return data_json
-    
 
 
-def save_data(data_dict):
-    # save to file
-    with open(path_data / file_data, "w") as fh:
+def save_data(data_dict: dict, path_of_file: pathlib.Path) -> None:
+    """Save data_dict to file."""
+    with open(path_of_file, 'w') as fh:
         json.dump(data_dict, fh)
 
 
 if __name__ == '__main__':
-    # run_http_server()
-    # run_form_handler_server(UDP_IP, UDP_PORT)
+
     thread_http_server = Thread(target=run_http_server)
     thread_http_server.start()
 
     thread_form_handler_server = Thread(target=run_form_handler_server, args=(UDP_IP, UDP_PORT))
     thread_form_handler_server.start()
-
-
